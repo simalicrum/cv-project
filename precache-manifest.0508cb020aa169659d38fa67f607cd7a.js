3ca0483cad61bd5655873e8233e762aceb10c70c
Robert Reinert <robert.reinert@gmail.com>
2020-10-08 12:10:19 -0700

    Updates

---
+++ precache-manifest.0508cb020aa169659d38fa67f607cd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18812e30ea33727d072ca6b7c1f016aa",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "e4132f9a8fb8c3666009",
+    "url": "/cv-project/static/css/main.177a71bf.chunk.css"
+  },
+  {
+    "revision": "bb1196733be9ea2a3fab",
+    "url": "/cv-project/static/js/2.9f679b0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-project/static/js/2.9f679b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4132f9a8fb8c3666009",
+    "url": "/cv-project/static/js/main.e4831bae.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
